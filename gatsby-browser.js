569a31af3a0d0a9536ac7c70188b62e3bed66daf
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-11-22 10:48:49 -0800

    Finished refactoring styles to adhere to ThemeUI specifications.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { StoreProvider } from "./src/context/store-context";
-import "./src/styles/variables.css";
 import "./src/styles/layout.css";
 
 export const wrapRootElement = ({ element }) => (
--- src/components/product-listing.js
+++ src/components/product-listing.js
@@ -3,7 +3,6 @@ import { jsx, Flex, Grid } from 'theme-ui';
 import * as React from 'react';
 import ProductCard from './product-card';
 import { productListingStyles } from '../utils';
-import { listingContainerStyle, listingContainerWrapper } from './product-listing.module.css';
 
 export function ProductListing({ products = [] }) {
   return (
