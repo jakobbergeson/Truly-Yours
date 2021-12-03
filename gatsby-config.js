17542cc05e5ec66bd6c05ff6c00741527aec3f48
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-12-03 10:40:59 -0800

    refactored cart width on mobile.

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: ['serif:200,300,400,500,600,700,800', 'Cinzel:200,300,400,500,600,700,800', ' Roboto',]
+          families: ['Serif:200,300,400,500,600,700,800', 'Cinzel:200,300,400,500,600,700,800', ' Roboto',]
         }
       }
     },
